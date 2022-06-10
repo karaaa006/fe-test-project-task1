3b986d02c950a6fd2b7924cf02f001568004adda
Oleh <olegkaraaa@gmail.com>
2022-06-11 01:35:18 +0300

    task done

--- src/components/list/list.js
+++
@@ -1,1 +0,0 @@
-console.log('List is here.');
---
+++ src/components/tabs/tabs.js
@@ -0,0 +1,28 @@
+const tabsNavigation = document.querySelectorAll(".tabs__default-radio");
+const activeNavigationTab = document.querySelector(
+  ".tabs__default-radio:checked"
+);
+const tabs = document.querySelectorAll(".tabs__tab-content");
+const tabNavigationList = document.querySelector(".tabs__list");
+
+const activeTab = [...tabs].find(
+  (item) => item.dataset.tabname === activeNavigationTab.value
+);
+
+activeTab.classList.add("active");
+
+const handleTabChange = (e) => {
+  const { value } = e.target;
+
+  const prevActiveTab = document.querySelector(".tabs__tab-content.active");
+
+  prevActiveTab.classList.remove("active");
+
+  const activeTab = [...tabs].find((item) => item.dataset.tabname === value);
+
+  activeTab.classList.add("active");
+};
+
+[...tabsNavigation].forEach((item) =>
+  item.addEventListener("change", handleTabChange)
+);
